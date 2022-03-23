83321a32d898222dc56ec2eabd4e89dcd0cee0ba
Lenfumé Julien <julabina@hotmail.fr>
2022-03-23 10:45:14 +0100

    css cards

--- src/Components/AppComponents/MovieCard.js
+++ src/Components/AppComponents/MovieCard.js
@@ -4,7 +4,10 @@ const MovieCard = (props) => {
     return (
         <li key={props.id} className='movieCard'>
             <div className="movieCard_poster">
+                {(props.poster !== null) ? 
                 <img src={"https://image.tmdb.org/t/p/w200" + props.poster} alt={"affiche du film" + props.title} />
+                : <div className="tvCard_poster_noImg"><p>Pas d'aperçu</p></div>
+                }
             </div>
             <div className='movieCard_infos'>
                 {props.fromHome ? <a href={'./movie/ref_=' + props.movieId}><h2>{props.title}</h2></a> : <a href={'./ref_=' + props.movieId}><h2>{props.title}</h2></a>}
--- src/Components/AppComponents/TvCard.js
+++ src/Components/AppComponents/TvCard.js
@@ -4,7 +4,10 @@ const TvCard = (props) => {
     return (
         <li key={props.id} className='tvCard'>
             <div className="tvCard_poster">
+                {(props.poster !== null) ? 
                 <img src={"https://image.tmdb.org/t/p/w200" + props.poster} alt={"affiche de la serie " + props.title} />
+                : <div className="tvCard_poster_noImg"><p>Pas d'aperçu</p></div>
+                }
             </div>
             <div className='tvCard_infos'>
                 {props.fromHome ? <a href={'./tv/ref_=' + props.tvId}><h2>{props.title}</h2></a> : <a href={'./ref_=' + props.tvId}><h2>{props.title}</h2></a>}
