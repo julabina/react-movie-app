457e3eb4787b8dd9458da93b3ff5ae1623567648
Lenfumé Julien <julabina@hotmail.fr>
2022-03-23 14:24:39 +0100

    modify cards link

--- src/Components/AppComponents/MovieCard.js
+++ src/Components/AppComponents/MovieCard.js
@@ -2,19 +2,22 @@ import React from 'react';
 
 const MovieCard = (props) => {
     return (
+        <>
+        <a href={props.fromHome ? ('./movie/ref_=' + props.movieId) : ('./ref_=' + props.movieId)}>
         <li key={props.id} className='movieCard'>
             <div className="movieCard_poster">
                 {(props.poster !== null) ? 
                 <img src={"https://image.tmdb.org/t/p/w200" + props.poster} alt={"affiche du film" + props.title} />
                 : <div className="tvCard_poster_noImg"><p>Pas d'aperçu</p></div>
-                }
+            }
             </div>
             <div className='movieCard_infos'>
-                {props.fromHome ? <a href={'./movie/ref_=' + props.movieId}><h2>{props.title}</h2></a> : <a href={'./ref_=' + props.movieId}><h2>{props.title}</h2></a>}
+                <h2>{props.title}</h2>
                 <h3>{props.release}</h3>
                 <p>{props.overview}</p>
             </div>
-        </li>
+        </li></a>
+        </>
     );
 };
 
--- src/Components/AppComponents/TvCard.js
+++ src/Components/AppComponents/TvCard.js
@@ -2,18 +2,21 @@ import React from 'react';
 
 const TvCard = (props) => {
     return (
+        <>
+        <a href={props.fromHome ? ('./tv/ref_=' + props.tvId) : ('./ref_=' + props.tvId)}>
         <li key={props.id} className='tvCard'>
             <div className="tvCard_poster">
                 {(props.poster !== null) ? 
                 <img src={"https://image.tmdb.org/t/p/w200" + props.poster} alt={"affiche de la serie " + props.title} />
                 : <div className="tvCard_poster_noImg"><p>Pas d'aperçu</p></div>
-                }
+            }
             </div>
             <div className='tvCard_infos'>
-                {props.fromHome ? <a href={'./tv/ref_=' + props.tvId}><h2>{props.title}</h2></a> : <a href={'./ref_=' + props.tvId}><h2>{props.title}</h2></a>}
+                <h2>{props.title}</h2>
                 <p>{props.overview}</p>
             </div>
-        </li>
+        </li></a>
+        </>
     );
 };
 
